ab4966dfd1cd36f0eaae2a0734bd9c199d4dee5c
Devanegas <dieguitovan@gmail.com>
2020-02-06 22:40:01 -0700

    Add project files.

---
+++ GameOfDiego/Program.cs
@@ -0,0 +1,43 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameOfDiego
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var obj = new Name();
+            obj.name = "Diego3";
+            string json = JsonConvert.SerializeObject(obj);
+            using (var client = new HttpClient())
+            {
+                var response = await client.PostAsync(
+                    "http://daybellphotography.com/register",
+                     new StringContent(json, Encoding.UTF8, "application/json"));
+                if (response.IsSuccessStatusCode)
+                {
+                    var contents = await response.Content.ReadAsStringAsync();
+                    Console.WriteLine(contents);
+                }
+                else
+                {
+                    Console.WriteLine("Error");
+                }
+            }
+
+        }
+
+
+
+    }
+
+    public class Name
+    {
+        public string name { get; set; }
+    }
+}
