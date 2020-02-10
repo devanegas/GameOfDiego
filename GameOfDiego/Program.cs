8fe9a328ccb537d5b9620fa72ffb257d299d9e6f
Devanegas <dieguitovan@gmail.com>
2020-02-09 17:53:41 -0700

    Can now update every second

--- GameOfDiego/Program.cs
+++ GameOfDiego/Program.cs
@@ -3,17 +3,51 @@ using System;
 using System.Net;
 using System.Net.Http;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace GameOfDiego
 {
-    class Program
+    public class Program
     {
         static async Task Main(string[] args)
         {
-            var obj = new Name();
-            obj.name = "DiegoUltimate";
-            string json = JsonConvert.SerializeObject(obj);
+            var name = new Name();
+            name.name = "Davanegas8";
+            var contents = await GetToken(name);
+
+            if (contents == "Error")
+            {
+                Console.WriteLine("Error");
+            }
+            else
+            {
+                var startTimeSpan = TimeSpan.Zero;
+                var periodTimeSpan = TimeSpan.FromSeconds(1);
+                var token = JsonConvert.DeserializeObject<Token>(contents);
+                var updateReq = new UpdateRequest();
+                updateReq.token = token.token;
+                updateReq.clientStatus = "waiting";
+                var response = "";
+
+                var timer = new System.Threading.Timer(async (e) =>
+                {
+                    response = await PostToken(updateReq);
+                    Console.WriteLine(response);
+                    Console.WriteLine("Solving the board....");
+                    Thread.Sleep(2000);
+                }, null, startTimeSpan, periodTimeSpan);
+
+                Console.ReadLine();
+                
+            }
+
+        }
+
+        private static async Task<string> GetToken(Name name)
+        {
+            
+            string json = JsonConvert.SerializeObject(name);
             using (var client = new HttpClient())
             {
                 var response = await client.PostAsync(
@@ -22,22 +56,50 @@ namespace GameOfDiego
                 if (response.IsSuccessStatusCode)
                 {
                     var contents = await response.Content.ReadAsStringAsync();
-                    Console.WriteLine(contents);
+                    return contents;
                 }
                 else
                 {
-                    Console.WriteLine("Error, name is already taken");
+                    return "Error";
                 }
             }
-
         }
 
-
-
+        private static async Task<string> PostToken(UpdateRequest updateReq)
+        {
+            string json = JsonConvert.SerializeObject(updateReq);
+            using (var client = new HttpClient())
+            {
+                var response = await client.PostAsync(
+                    "http://daybellphotography.com/update",
+                     new StringContent(json, Encoding.UTF8, "application/json"));
+                if (response.IsSuccessStatusCode)
+                {
+                    var contents = await response.Content.ReadAsStringAsync();
+                    return contents;
+                }
+                else
+                {
+                    return "Error updating";
+                }
+            }
+        }
     }
 
     public class Name
     {
         public string name { get; set; }
+    }
+
+    public class Token
+    {
+        public string name { get; set; }
+        public string token { get; set; }
+    }
+
+    public class UpdateRequest
+    {
+        public string token { get; set; }
+        public string clientStatus { get; set; }
     }
 }
